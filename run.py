556e74ec3d9578171d64a6ffa5a771ffbdf250c3
Adam Moss <adam.moss@nottingham.ac.uk>
2022-05-12 12:21:17 +0100

    Update run.py

--- run.py
+++ run.py
@@ -21,6 +21,7 @@ if __name__ == '__main__':
     front_wheels = None
     back_wheels = None
     if args.mode in ['drive', 'ludicrous', 'plaid']:
+        print('Using config file %s' % args.picar_debug)
         try:
             import picar
             picar.setup()
